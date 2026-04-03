339a48e2108bd6250b25417153ca1c4de197b1bb
kaieye <623917753@qq.com>
2026-04-03 12:46:03 +0800

    chore(harmony): stop tracking build caches

--- apps/harmony/entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/common/utils/VideoPlaybackTarget.ts
+++
@@ -1,7 +0,0 @@
-export type VideoPlaybackTarget = 'inline' | 'dedicated';
-export function resolveVideoPlaybackTarget(isYouTubeVideo: boolean, isDirectVideoFile: boolean): VideoPlaybackTarget {
-    if (isYouTubeVideo && !isDirectVideoFile) {
-        return 'dedicated';
-    }
-    return 'inline';
-}
--- apps/harmony/entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/common/utils/YouTubePlaybackMode.ts
+++
@@ -1,6 +0,0 @@
-export function shouldOpenYouTubeExternallyOnFallback(): boolean {
-    return false;
-}
-export function shouldUseYouTubeWebFallback(): boolean {
-    return true;
-}
